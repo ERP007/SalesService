a355f347f51ca5b649de1391a3cb082b7d30a900
seungwoo <rio970319@gmail.com>
2026-06-03 17:53:56 +0900

    feature(ERP-86): controller 작성 (#10)

---
+++ src/main/java/com/fallguys/salesservice/adapter/inbound/web/SalesOrderController.java
@@ -0,0 +1,41 @@
+package com.fallguys.salesservice.adapter.inbound.web;
+
+import com.fallguys.salesservice.adapter.inbound.web.dto.CreateDraftSalesOrderRequest;
+import com.fallguys.salesservice.adapter.inbound.web.dto.CreateSalesOrderRequest;
+import com.fallguys.salesservice.adapter.inbound.web.dto.CreateSalesOrderResponse;
+import com.fallguys.salesservice.application.port.inbound.CreateSalesOrderUseCase;
+import com.fallguys.salesservice.domain.model.SalesOrder;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sales-orders")
+@RequiredArgsConstructor
+public class SalesOrderController {
+
+    private final CreateSalesOrderUseCase createSalesOrderUseCase;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateSalesOrderResponse create(
+            // TODO: Gateway가 주입하는 사용자 식별 헤더 키 확정 필요
+            @RequestHeader("X-User-Id") String userId,
+            @Valid @RequestBody CreateSalesOrderRequest request
+    ) {
+        SalesOrder salesOrder = createSalesOrderUseCase.create(request.toCommand(userId));
+        return CreateSalesOrderResponse.from(salesOrder);
+    }
+
+    @PostMapping("/drafts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateSalesOrderResponse createDraft(
+            // TODO: Gateway가 주입하는 사용자 식별 헤더 키 확정 필요
+            @RequestHeader("X-User-Id") String userId,
+            @Valid @RequestBody CreateDraftSalesOrderRequest request
+    ) {
+        SalesOrder salesOrder = createSalesOrderUseCase.create(request.toCommand(userId));
+        return CreateSalesOrderResponse.from(salesOrder);
+    }
+}
