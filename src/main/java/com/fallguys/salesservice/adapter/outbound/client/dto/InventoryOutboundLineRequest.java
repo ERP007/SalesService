52c79c07819dd32692bad82649f8e957f6742b8f
seungwoo <rio970319@gmail.com>
2026-06-08 17:28:26 +0900

    feature(ERP-147): outbound DTO 선언 (#35)

---
+++ src/main/java/com/fallguys/salesservice/adapter/outbound/client/dto/InventoryOutboundLineRequest.java
@@ -0,0 +1,10 @@
+package com.fallguys.salesservice.adapter.outbound.client.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record InventoryOutboundLineRequest(
+        String sku,
+        int quantity,
+        @JsonProperty("source_line_no") Long sourceLineNo
+) {
+}
